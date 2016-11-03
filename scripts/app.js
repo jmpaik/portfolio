1ce77730029f3f3a4a25509649126f959986c9f1
JINHO PAIK <JMPAIK83@GMAIL.COM>
2016-11-03 10:07:13 -0700

    pushing commit for maelle

--- scripts/app.js
+++ scripts/app.js
@@ -1,39 +1,56 @@
-function Project (options) {
-  for (var keys in options) {
-    this[keys] = options[keys];
+(function(module) {
+
+  function Project (options) {
+    for (var keys in options) {
+      this[keys] = options[keys];
+    }
   }
-}
 
-Project.allProjects = [];
+  Project.allProjects = [];
 
-Project.prototype.toHtml = function(source) {
-  var templateRender = Handlebars.compile($(source).text());
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
-  this.body = (this.body);
-  return templateRender(this);
-};
+  Project.prototype.toHtml = function(source) {
+    var templateRender = Handlebars.compile($(source).text());
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+    this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+    this.body = (this.body);
+    return templateRender(this);
+  };
 
-Project.loadAll = function(inputData) {
-  inputData.sort(function(a,b) {
-    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-  })
-  .forEach(function(ele) {
-    Project.allProjects.push(new Project(ele));
-  });
-};
-
-Project.fetchAll = function() {
-  if (localStorage.projectsList) {
-    var projectsList = JSON.parse(localStorage.getItem('projectsList'));
-    Project.loadAll(projectsList);
-    appView.renderIndexPage();
-  } else {
-
-    $.getJSON('../database/projectsList.json', function(data){
-      localStorage.setItem('projectsList', JSON.stringify(data));
-      Project.loadAll(data);
-      appView.renderIndexPage();
+  Project.loadAll = function(inputData) {
+    inputData.sort(function(a,b) {
+      return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+    })
+    .forEach(function(ele) {
+      Project.allProjects.push(new Project(ele));
     });
-  }
-};
+  };
+
+  Project.fetchAll = function() {
+    if (localStorage.projectsList) {
+      var projectsList = JSON.parse(localStorage.getItem('projectsList'));
+      Project.loadAll(projectsList);
+      appView.renderIndexPage();
+    } else {
+
+      $.getJSON('../database/projectsList.json', function(data){
+        localStorage.setItem('projectsList', JSON.stringify(data));
+        Project.loadAll(data);
+        appView.renderIndexPage();
+      });
+    }
+    
+    Project.numWordsAll = function() {
+      return Project.allProjects.map(function(project) {
+      // NOTE: Grab the word count from each project body.
+        return project.body.split(' ').length;
+      })
+      // TODO:DONE complete this reduce to get a grand total word count
+      .reduce(function(acc, cur, idx, array) {
+        acc = acc + cur;
+        return acc;
+      }, 0);
+    };
+  };
+
+  module.Project = Project;
+})(window);
