de1f76c62befcfe37a8e6177eb098bfc325bb57b
JINHO PAIK <JMPAIK83@GMAIL.COM>
2016-11-08 11:50:16 -0800

    debugged page.js

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,5 +1,5 @@
 page('/', homeController.reveal);
 page('/curriculumn-vitae', cvController.reveal);
-page('/projects', projectsController);
-page('/contact-me', contactmeController);
+page('/projects', projectsController.reveal);
+page('/contact-me', contactmeController.reveal);
 page();
