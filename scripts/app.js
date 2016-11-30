6f8df927ad2cef0c5d8fde8b099b9b0d36c62d1d
JINHO PAIK <JMPAIK83@GMAIL.COM>
2016-11-30 11:21:52 -0800

    more styling

--- scripts/app.js
+++ scripts/app.js
@@ -11,7 +11,7 @@
   Project.prototype.toHtml = function(source) {
     var templateRender = Handlebars.compile($(source).text());
     this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-    this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+    this.publishStatus = this.publishedOn ? 'Published ' + this.daysAgo + ' days ago' : '(draft)';
     this.body = (this.body);
     return templateRender(this);
   };
